e39ace6a66100b39b70463bc7300d6c2c756a026
Jenie2115 <jenie.romero15@gmail.com>
2024-10-31 12:36:21 -0400

    Add files via upload

---
+++ Operadores.js
@@ -0,0 +1,63 @@
+//sumas
+
+function sumar(){
+
+var numero1= document.getElementById("numero1").value;
+
+var numero2= document.getElementById("numero2").value;
+ 
+ alert( suma= parseInt(numero1) + parseInt(numero2))
+ 
+}
+//restar
+function restar(){
+
+var numero3= document.getElementById("numero3").value;
+
+var numero4= document.getElementById("numero4").value;
+ 
+ alert( resta= parseInt(numero3) - parseInt(numero4))
+ 
+}
+//multiplicación
+function multiplicar(){
+
+var numero5= document.getElementById("numero5").value;
+
+var numero6= document.getElementById("numero6").value;
+ 
+ alert( multiplicación= parseInt(numero5) * parseInt(numero6))
+ 
+}
+//dividir
+
+function dividir(){
+
+var numero7= document.getElementById("numero7").value;
+
+var numero8= document.getElementById("numero8").value;
+ 
+ alert( dividir= parseInt(numero7) / parseInt(numero8))
+ 
+}
+
+//verdadero o falso
+//function resultado(){
+
+//var v= document.getElementById("v").value;
+//var a= document.getElementById("a").value;
+
+// let resultado;   
+// if (v > a) //alert(resultado="verdadero");}
+    
+// else if(v>=a){
+     
+//  alert(resultado="igual");   
+  
+// }else{
+ //   alert(resultado="falso");
+//alert(MenorQue=parseInt(i)<parseInt(a))      
+//}      
+ //return resultado;  
+//}
+//
